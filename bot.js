9f89cec9fefeab5810f4397ce6ce8553f60c83cc
ProRehber <36857933+ProRehber@users.noreply.github.com>
2018-04-21 16:20:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,34 @@ client.on('message', msg => {
   if (msg.content.toLowerCase() === prefix + 'ping') {
     msg.reply('Pong! **' + client.ping + '** ms');
   }
+//Koruma
+client.on("message", msg => {
+  if (msg.content.toLowerCase().match(/(discord\.gg\/)|(discordapp\.com\/invite\/)/g) && !msg.author.bot && msg.channel.type === "text" && msg.channel.permissionsFor(msg.guild.member(client.user)).has("MANAGE_MESSAGES")) {
+    msg.delete(30).then(deletedMsg => {
+      deletedMsg.reply(":x: Discord davet linki paylaştığını algıladık. Bu sunucu ProRehberTR tarafından korunmakta. :x: ").catch(e => {
+        console.error(e);
+      });
+    }).catch(e => {
+      console.error(e);
+    });
+  }
+});
+//Koruma
+//temizleme
+  if (msg.content.toLowerCase() === prefix + 'temizle') {
+exports.run = function(client, message, args) {
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!");
+if(!args[0]) return message.channel.send("🚫 **Lütfen Silinicek Mesaj Miktarını Yazın.!** 🚫");
+message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(` ${args[0]} Adet Mesajı Sildim. ✅`).then(msg => msg.delete(5000));
+})
+}
+//temizleme
+//yardım
+  if (msg.content.toLowerCase() === prefix + 'yardım') {
+    msg.channel.sendMessage("Ben özel yapım bir botum ve 7/24 açığım.İşte komutlarım ve özelliklerim ; \np!temizle = Belirliten miktar kadar mesajı siler. \np!ping = Pingimi gösterir. \nAyrıca Reklam korumasıda vardır.");
+  }
+//yardım
   if (msg.content.toLowerCase() === prefix + 'reboot') {
     if (msg.author.id !== process.env.sahip) {
       msg.reply('Benim yapımcım değilsin!');
