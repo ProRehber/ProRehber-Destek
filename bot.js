25abcbfce9c003f2c412de8fc2da256ba7cb8c83
ProRehber <36857933+ProRehber@users.noreply.github.com>
2018-04-21 19:42:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -15,88 +16,10 @@ client.on('message', msg => {
   if (!msg.content.startsWith(prefix)) {
 	  return;
   }
-
-const log = message => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-};
-
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
-  if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
-    let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.name}.`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.name);
-    });
-  });
-});
-
-client.reload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
-
-client.load = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
-
-client.unload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
-
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('Aleyküm selam,  hoş geldin');
+  if (msg.content.toLowerCase() === prefix + 'ping') {
+    msg.reply('Pong! **' + client.ping + '** ms');
   }
-});
-//yardım
-  if (msg.content.toLowerCase() === prefix + 'yardım') {
-    msg.channel.sendMessage("Ben özel yapım bir botum ve 7/24 açığım.İşte komutlarım ve özelliklerim ; \np!temizle = Belirliten miktar kadar mesajı siler. \np!ping = Pingimi gösterir. \nAyrıca Reklam korumasıda vardır.");
-  }
-//yardım
-//Küfür-Reklam Koruması
+//Koruma
 client.on("message", msg => {
   if (msg.content.toLowerCase().match(/(discord\.gg\/)|(discordapp\.com\/invite\/)/g) && !msg.author.bot && msg.channel.type === "text" && msg.channel.permissionsFor(msg.guild.member(client.user)).has("MANAGE_MESSAGES")) {
     msg.delete(30).then(deletedMsg => {
@@ -108,29 +31,28 @@ client.on("message", msg => {
     });
   }
 });
-//hastebin
-//bilmem nere
-client.elevation = message => {
-  if(!message.guild) {
-	return; }
-  let permlvl = 0;
-  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-  if (message.author.id === ayarlar.sahip) permlvl = 4;
-  return permlvl;
-};
-
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-// client.on('debug', e => {
-//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
-// });
-
-client.on('warn', e => {
-  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
-});
-
-client.on('error', e => {
-  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+//Koruma
+//temizleme
+  if (msg.content.toLowerCase() === prefix + 'temizle') {
+    msg.channel.bulkDelete(100);
+    msg.channel.sendMessage("100 adet mesaj silindi!");
+  }
+//temizleme
+//yardım
+  if (msg.content.toLowerCase() === prefix + 'yardım') {
+    msg.channel.sendMessage("Ben özel yapım bir botum ve 7/24 açığım.İşte komutlarım ve özelliklerim ; \np!temizle = 100 adet mesaj siler. \np!ping = Pingimi gösterir. \nAyrıca Reklam korumasıda vardır.");
+  }
+//yardım
+  if (msg.content.toLowerCase() === prefix + 'reboot') {
+    if (msg.author.id !== process.env.sahip) {
+      msg.reply('Benim yapımcım değilsin!');
+    } else {
+      msg.channel.sendMessage(`Bot yeniden başlatılıyor...`).then(msg => {
+      console.log(`BOT: Bot yeniden başlatılıyor...`);
+      process.exit(0);
+    })
+   }
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
